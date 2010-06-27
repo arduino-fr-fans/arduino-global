6339a77823d4b4066e7cf36a106dcf293a950d43
Nicolas Maupu <nmaupu@excilys.com>
2010-06-27 19:24:22 +0200

    Add the ability to control leds from bash (interactively and by using pipe)

--- led-wall/serial_send.sh
+++ led-wall/serial_send.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-while [ 1 ]
-do
-  read -n 1 -s val
-  echo ">${val}"
-  echo -n "${val}" > /dev/ttyUSB0
-done
+## If stdin is empty, we read from terminal
+if [ -t 0 ]
+then
+  while [ 1 ]
+  do
+    read -n 1 -s val
+    echo ">${val}"
+    echo -n "${val}" > /dev/ttyUSB0
+  done
+else
+  ## we read data from stdin
+  var=`cat -`
+  echo -n "${var}" > /dev/ttyUSB0
+fi
